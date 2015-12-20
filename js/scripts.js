c64e295a08acb4c9d4e6d280517bc43cc2f03b15
Jason Arnold <jason@thejasonfile.com>
2015-12-20 13:14:23 -0500

    JS collecting phase information, working on timer

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,82 @@ var numPoms = document.getElementById('num_of_pomodoros');
 var startButton = document.getElementById('start');
 var pauseButton = document.getElementById('pause');
 var clearButton = document.getElementById('clear');
+var skipButton = document.getElementById('skip');
 
 var currentPomTime = document.getElementById('pomodoro_time')
 
 //create initial pomodoro object
-var pomodoro_obj = {
-	length: 25,
-	sbreak: 5,
-	lbreak: 30,
-	numPoms: 4
-}
+var pomodoro_obj = {}
 
 var setPomValues = function() {
 	console.log('start pomodoro...');
-	//take values from input fields
+	//take values from input fields and add them to object
 	pomodoro_obj.length = pomLength.value;
 	pomodoro_obj.sbreak = shortBreakLength.value;
 	pomodoro_obj.lbreak = longBreakLength.value;
+	pomodoro_obj.currentPhase = 0;
 	pomodoro_obj.numPoms = numPoms.value;
+	//set phaseArray
+	setPhaseArray(pomodoro_obj.numPoms);
 	//set HTML on divs
 	currentPomTime.innerHTML = pomodoro_obj.length;
-	//start the pomodoro
-	startPom();
-}
-
-var startPom = function() {
-	console.log('start pomodoro...');
-	//start countdown
 
 	//input fields become labels
-	
+
+	//start the pomodoro
+	getCurrentPhase();
 }
+
+var setPhaseArray = function(numPoms) {
+	console.log('setting phase array...')
+	pomodoro_obj.phaseArray = [];
+	pomodoro_obj.phaseArray.push('pomodoro');
+	for (var i = 0; i < numPoms; i++) {
+		pomodoro_obj.phaseArray.push('sbreak' + i);
+		pomodoro_obj.phaseArray.push('pomodoro');
+	}
+	pomodoro_obj.phaseArray.push('lbreak');
+}
+
+var getCurrentPhase = function(phaseName) {
+	console.log('get current phase...');
+	//get current phase and time
+	var phaseName = pomodoro_obj.phaseArray[pomodoro_obj.currentPhase];
+	var phaseMin = '';
+	var phaseSec = 0;
+	switch(phaseName) {
+		case 'sbreak':
+			phaseMin = pomodoro_obj.sbreak;
+			break;
+		case 'lbreak':
+			phaseMin = pomodoro_obj.lbreak;
+			break;
+		default:
+			phaseMin = pomodoro_obj.length;
+	}
+	currentPomTime.innerHTML = phaseMin + ':' + ("0" + phaseSec);
+	startPhase(pomLength.value);
+}
+
+var startPhase = function() {
+	console.log('start phase...');
+	//start countdown
+		//while time > 0
+			//decrease number by 1 every 1 second
+		//until time = 0
+			//move on to next phase
+
+	//when current phase finishes
+		//increase phase number by 1
+		//start phase
+}
+
+var skipPhase = function() {
+	//skip current phase
+	console.log('skip current phase...')
+	//increase phase number by 1
+	//get current phase information
+	}
 
 var pausePom = function() {
 	//pause countdown
@@ -48,12 +93,13 @@ var clearPom = function() {
 	var pomItems = document.getElementById('pomodoro_items');
 	//stop countdown
 
-	//clear countdown
+	//clear timers
 	currentPomTime.innerHTML = '';
-	//clear object values
+	//reset object values
 	for (var key in pomodoro_obj) {
 		pomodoro_obj[key] = 0;
 	}
+	pomodoro_obj.phaseArray = [];
 	//clear input values
 	for (var i = 0; i < pomItems.children.length; i++) {
 		pomItems.children[i].querySelector('input[type=text]').value = '';
@@ -64,3 +110,4 @@ var clearPom = function() {
 startButton.addEventListener('click', setPomValues);
 pauseButton.addEventListener('click', pausePom);
 clearButton.addEventListener('click', clearPom);
+skipButton.addEventListener('click', skipPhase);
